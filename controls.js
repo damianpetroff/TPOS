7033945a831cd48fd1c6d064dbfc59be05898a6b
quentin.michel <quentin.michel@he-arc.ch>
2019-02-25 16:11:13 +0100

    add UI buttons
    need to complete and debug functions
    and need to set the speed of Entities

--- controls.js
+++ controls.js
@@ -1,3 +1,5 @@
+var stop = false;
+
 window.addEventListener('load', function () {
 
     let bufferQte = document.getElementById("bufferQte");
@@ -9,6 +11,16 @@ window.addEventListener('load', function () {
 
     bufferQte.addEventListener("input", setBufferQte);
     bufferQte.addEventListener("input", function(e) { updateLabel(e.srcElement, bufferQteLabel); });
+
+
+    let btnSync = document.getElementById("btnSync");
+    let btnRestart = document.getElementById("btnRestart");
+    let btnStop = document.getElementById("btnStop");
+
+    btnSync.addEventListener("click", function(){ synchronize(); });
+    btnRestart.addEventListener("click", function(){ restart(bufferQte.value); });
+    btnStop.addEventListener("click", function(){ logicStopButton(); });
+
 
     updateLabels();
 
@@ -27,4 +39,31 @@ function setBufferQte(e)
 function updateLabel(range, label)
 {
     label.innerHTML = range.value;
+}
+
+function logicStopButton()
+{
+    if(stop)
+    {
+        stop = false;
+        btnStop.innerText = "Stop";
+        loop();
+    }
+    else
+    {
+        stop = true;
+        btnStop.innerText = "Resume";
+        noLoop();
+    }
+}
+
+function restart(bufferQuantity)
+{
+    // quantity is true, but donno why it's bugged
+    reset(bufferQuantity);
+}
+
+function synchronize()
+{
+  // TODO
 }
