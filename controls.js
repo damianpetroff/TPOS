cb9664e081689daafc2968f0ae5cd3944b868cc5
Raphaël Margueron <raphael.margueron@he-arc.ch>
2019-03-11 15:16:07 +0100

    auto format controls + change vars to lets

--- controls.js
+++ controls.js
@@ -1,4 +1,4 @@
-var stop = false;
+let stop = false;
 
 window.addEventListener('load', function() {
 
@@ -18,9 +18,15 @@ window.addEventListener('load', function() {
     let btnRestart = document.getElementById("btnRestart");
     let btnStop = document.getElementById("btnStop");
 
-    btnSync.addEventListener("click", function(){ synchronize(); });
-    btnRestart.addEventListener("click", function(){ restart(bufferQte.value); });
-    btnStop.addEventListener("click", function(){ logicStopButton(); });
+    btnSync.addEventListener("click", function() {
+        synchronize();
+    });
+    btnRestart.addEventListener("click", function() {
+        restart(bufferQte.value);
+    });
+    btnStop.addEventListener("click", function() {
+        logicStopButton();
+    });
 
     updateLabels();
 
@@ -41,70 +47,62 @@ function updateLabel(range, label) {
 }
 
 function changeEntitieSpeed(element) {
-  producerConsumer.entities[parseInt(element.id)].speed = parseInt(element.value);
+    producerConsumer.entities[parseInt(element.id)].speed = parseInt(element.value);
 }
 
 function entitesSpeedLogic() {
-  //remove old rows
-  var elmtTable = document.getElementById('table_entities');
-  var tableRows = elmtTable.getElementsByTagName('tr');
-  var rowCount = tableRows.length;
+    //remove old rows
+    let elmtTable = document.getElementById('table_entities');
+    let tableRows = elmtTable.getElementsByTagName('tr');
+    let rowCount = tableRows.length;
 
-  for (var x=rowCount-1; x>0; x--) {
-     elmtTable.removeChild(tableRows[x]);
-  }
+    for (let x = rowCount - 1; x > 0; x--) {
+        elmtTable.removeChild(tableRows[x]);
+    }
 
-  // add new rows
-  var entitiesQte = int(bufferQte.value)+1;
-  for(let i=0;i<entitiesQte;i++)
-  {
-      var tr = document.createElement("tr");
-      var tdEntities = document.createElement("td");
-      tdEntities.style = "text-align:center";
-      tdEntities.innerHTML = i;
-      var tdSpeed = document.createElement("td");
-      tdSpeed.style = "text-align:center";
-      var input = document.createElement("input");
-      input.id = i;
-      input.type = "number";
-      input.min = 1;
-      input.max = 10;
-      input.value = producerConsumer.entities[i].speed;
-      input.addEventListener("change", function(e) {
-        changeEntitieSpeed(e.srcElement);
-      });
-      tdSpeed.appendChild(input);
+    // add new rows
+    let entitiesQte = int(bufferQte.value) + 1;
+    for (let i = 0; i < entitiesQte; i++) {
+        let tr = document.createElement("tr");
+        let tdEntities = document.createElement("td");
+        tdEntities.style = "text-align:center";
+        tdEntities.innerHTML = i;
+        let tdSpeed = document.createElement("td");
+        tdSpeed.style = "text-align:center";
+        let input = document.createElement("input");
+        input.id = i;
+        input.type = "number";
+        input.min = 1;
+        input.max = 10;
+        input.value = producerConsumer.entities[i].speed;
+        input.addEventListener("change", function(e) {
+            changeEntitieSpeed(e.srcElement);
+        });
+        tdSpeed.appendChild(input);
 
-      tr.appendChild(tdEntities);
-      tr.appendChild(tdSpeed);
+        tr.appendChild(tdEntities);
+        tr.appendChild(tdSpeed);
 
-      elmtTable.appendChild(tr);
-  }
+        elmtTable.appendChild(tr);
+    }
 }
 
-function logicStopButton()
-{
-    if(stop)
-    {
-        stop = false;
+function logicStopButton() {
+    if (stop) {
         btnStop.innerText = "Stop";
         loop();
-    }
-    else
-    {
-        stop = true;
+    } else {
         btnStop.innerText = "Resume";
         noLoop();
     }
+    stop ^= true;
 }
 
-function restart(bufferQuantity)
-{
+function restart(bufferQuantity) {
     // quantity is true, but donno why it's bugged
     reset(bufferQuantity);
 }
 
-function synchronize()
-{
-  // TODO
+function synchronize() {
+    // TODO
 }
