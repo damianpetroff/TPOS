f3f7f04bbdbebbed8ce393df2904e3f484d29a03
damianpetroff <damian.petroff@he-arc.ch>
2019-03-11 15:14:38 +0100

    UI

--- controls.js
+++ controls.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function () {
+    document.getElementById("lastModified").innerHTML = getFormattedDate(new Date(document.lastModified));
 
     let bufferQte = document.getElementById("bufferQte");
     let bufferQteLabel = document.getElementById("bufferQteLabel");
@@ -27,4 +28,30 @@ function setBufferQte(e)
 function updateLabel(range, label)
 {
     label.innerHTML = range.value;
+}
+
+function getFormattedDate(date) {
+  var monthNames = [
+    "January", "February", "March",
+    "April", "May", "June", "July",
+    "August", "September", "October",
+    "November", "December"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year + ' at ' + forceTwoDigits(hour) + ':' + forceTwoDigits(min);
+}
+
+function forceTwoDigits(number)
+{
+    if(number<10)
+    {
+        return '0'+str(number);
+    }
+    return number;
 }
--- producerConsumer.js
+++ producerConsumer.js
@@ -1,8 +1,10 @@
 let producerConsumer;
+let cWhidth = screen.width;
+let cHeight = screen.height;
 
 function setup() {
-    createCanvas(screen.width-18, screen.height-10);
-    producerConsumer = new ProducerConsumer(3, 1, 1);
+    let canvas = createCanvas(cWhidth, cHeight);
+    canvas.parent("draw");
     reset(DEFAULTVALUEBUFFERQTE);
 }
 
@@ -17,7 +19,54 @@ function draw() {
     producerConsumer.draw();
 }
 
-function mousePressed() {
-    let fs = fullscreen();
-    fullscreen(!fs);
+function clickOn() {
+	//Empty
+}
+
+function toggleFullScreen(){
+	console.info("Toggle Fullscreen");
+	var isInFullScreen = 
+		(document.fullscreenElement && document.fullscreenElement !== null) ||
+	    (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+	    (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+	    (document.msFullscreenElement && document.msFullscreenElement !== null);
+
+    if (!isInFullScreen)
+    {
+        if(canvas.requestFullscreen)
+        {
+          canvas.requestFullscreen();
+        }
+        else if(canvas.mozRequestFullScreen) 
+        {
+    	  canvas.mozRequestFullScreen();
+        } 
+        else if(canvas.webkitRequestFullScreen) 
+        {
+          canvas.webkitRequestFullScreen();
+        } 
+        else if(canvas.msRequestFullscreen) 
+        {
+          canvas.msRequestFullscreen();
+        }
+    } 
+    else 
+    {
+        if (document.exitFullscreen)
+        {
+            document.exitFullscreen();
+        }
+        else if (document.webkitExitFullscreen)
+        {
+            document.webkitExitFullscreen();
+        } 
+        else if (document.mozCancelFullScreen) 
+        {
+            document.mozCancelFullScreen();
+        } 
+        else if (document.msExitFullscreen)
+        {
+            document.msExitFullscreen();
+        }
+    }
 }
